a4cade329d7d4194006c1fb1a1f6df241edbe1d2
Sasikanth <sraghava@gunaatita.com>
2016-02-24 11:37:11 +0530

    first commit

---
+++ AnamolyDetection/Start.py
@@ -0,0 +1,22 @@
+from pyspark import SparkConf, SparkContext
+from pyspark.sql import SQLContext
+from pyspark.sql.types import *
+import os
+import sys
+
+
+def yarn_job():
+    train = sqlContext.read.format('com.databricks.spark.csv').options(header='true', inferschema='true').load('data/train.csv')
+    train.registerTempTable("train")
+    sqlContext.sql("select v22,count(target) from train group by v22").show()
+    print "Successfully launched PySpark, Press Enter to Finish the Session:"
+    raw_input()
+
+if __name__ == "__main__":
+    spark_home = os.environ.get("SPARK_HOME")
+    sys.path.insert(0, spark_home + "/python")
+    os.environ["PYSPARK_SUBMIT_ARGS"]='--master local --executor-memory 2g --packages com.databricks:spark-csv_2.10:1.3.0 pyspark-shell'
+    sys.path.insert(0, os.path.join(spark_home, "python/lib/py4j-0.8.2.1-src.zip"))
+    execfile(os.path.join(spark_home, "python/pyspark/shell.py"))
+    yarn_job()
+
